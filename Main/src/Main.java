7b44e118176ad840bc5cbe1095213bce7625fb8d
Anderson D Bernardo <Anderson.danttas@hotmail.com>
2025-08-19 21:15:40 -0300

    update

---
+++ Main/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       // int minhaIdade =  20;
+
+        boolean bool = true;
+
+        if (bool) {
+            System.out.println("Verdadeiro");
+        }else {
+            System.out.println("Falso");
+        }
+
+    }
+
+
+}
